4e6eacf91520b8c7b60568976e7ac047e4bb3ef4
Sandesh Naik <devsandeshnaik@gmail.com>
2020-02-17 20:45:14 +0530

    Added Project19.

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Sandesh on 15/02/20.
+//  Copyright © 2020 Sandesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
